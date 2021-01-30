9f3846838347cad28a713016b32811ea584bc390
AndersenCastaneda <34457834+AndersenCastaneda@users.noreply.github.com>
2021-01-30 17:08:11 -0500

    Add task 4

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static float vertical;
+    public static float horizontal;
+
+    private void Update() => GetInput();
+
+    private void GetInput()
+    {
+        vertical = Input.GetAxis("Vertical");       // W y S ó ↑ y ↓
+        horizontal = Input.GetAxis("Horizontal");   // A y D ó ← y →
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float moveSpeed = 10f;
+    public bool isOnGround = true;
+
+    private void Awake() => rb = gameObject.GetComponent<Rigidbody>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+            isOnGround = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void FixedUpdate() => Perform();
+
+    private void Perform()
+    {
+        if (isOnGround)
+            rb.MovePosition(rb.position + (new Vector3(InputManager.horizontal, 0, InputManager.vertical) * moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void Jump()
+    {
+        if (isOnGround)
+        {
+            isOnGround = false;
+            rb.AddForce(new Vector3(InputManager.horizontal, 5, InputManager.vertical), ForceMode.Impulse);
+        }
+    }
+
+    private void StopMovement()
+    {
+        rb.drag = 2;
+        this.enabled = false;
+    }
+}
