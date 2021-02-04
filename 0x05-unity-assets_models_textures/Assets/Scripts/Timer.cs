3d581760a824e7ce217bdbcc7be473d308fe1ae9
AndersenCastaneda <34457834+AndersenCastaneda@users.noreply.github.com>
2021-02-03 23:17:17 -0500

    add task 14

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    private float startTime;
+    int minutes;
+    float seconds;
+
+    private void OnEnable() => WinTrigger.OnWin += Inactive;
+    private void OnDisable() => WinTrigger.OnWin -= Inactive;
+
+    private void Start() => startTime = Time.time;
+
+    void Update()
+    {
+        float t = Time.time - startTime;
+        minutes = ((int)t / 60);
+        seconds = (t % 60);
+
+        if (seconds < 10f)
+            TimerText.text = $"{minutes}:0{seconds.ToString("f2").Replace(",", ".")}";
+        else
+            TimerText.text = $"{minutes}:{seconds.ToString("f2").Replace(",", ".")}";
+    }
+
+    private void Inactive()
+    {
+        TimerText.color = Color.green;
+        TimerText.fontSize = 60;
+        enabled = false;
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable 0649
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    [SerializeField] private Timer timer;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            timer.enabled = true;
+    }
+}
