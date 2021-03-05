6d4a1c24ae0145aaf8eb89b01058d2c569402c56
AndersenCastaneda <34457834+AndersenCastaneda@users.noreply.github.com>
2021-03-04 22:11:08 -0500

    Add character model

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable 0649
+using System.Collections;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private GameObject timerCanvas;
+    [SerializeField] private GameObject camera;
+
+    public void OnFinishedIntro()
+    {
+        camera.SetActive(true);
+        timerCanvas.SetActive(true);
+        playerController.enabled = true;
+        enabled = false;
+        gameObject.SetActive(false);
+    }
+}
